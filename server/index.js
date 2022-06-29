9983b1d1f3e7ed7340618669b8dabdaa28dc0234
rayen-abbes <rayenabbes0161@gmail.com>
2022-06-29 14:02:52 +0100

    server basics

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 const express = require ("express");
+// Run the app as a function
 const app = express();
+// Cors enable cross origin requrests
 const cors = require('cors');
+
 
 app.use(express.json())
 app.use(cors())
 
 let port = 5000;
 
+// Testing get requests on /products
 app.get('/products',(req,res) => {
     res.json({"name":"phone"})
 })
 
+// listenning on port
 app.listen(port,()=>{
     console.log('listening on port '+port)
 })
