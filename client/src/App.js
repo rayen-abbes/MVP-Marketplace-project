181465377a1c2585242bd4057cf146a19b4bdfff
rayen-abbes <rayenabbes0161@gmail.com>
2022-06-29 18:10:49 +0100

    test works

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,33 @@
 import React from "react";
 
 
-const App =() =>{    
+const App =() =>{
+    
     const elements = [
         
         <div>
             <h1 id="title">Marketplace</h1>
         </div>
     ]
-    /*
+    /* 
         * id
         * productName
-        * phoneNumber
         * details
+        * phoneNumber
         * price
         * picture
     */
+    let content = (
+        <div>
+            <img class="image3" src={require('./images/test.png')} alt="" width="300" height="400">
+            </img>
+            <h2>Phone</h2>
+            <p>This phone is one of the best here</p>
+            <p>22 222 222</p>
+            <p>300 dt</p>
+        </div>
+    )
+    elements.push(content)
     return elements;
 }
 
